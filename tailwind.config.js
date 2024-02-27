c207a3f6ecd1c652a2dbad364f17576a0fad7a2a
Abdulmalik <103919618+0300hrs@users.noreply.github.com>
2024-02-27 15:36:23 +0100

    Add files via upload
    Nice work

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        white: "#FFFFFF",
+        brown: "#84443E",
+        darkGray: "#1e4b3f",
+        darkBrown: "#211b1c",
+        veryDarkBrown: "#4f4819",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
